d69ff47e26e4fe1748e6b21868f2f67629f58440
Rodrigo <rbz991@hotmail.com>
2024-02-15 11:52:01 -0600

    Added criteria to end session

--- RenovacionInduccion/MainH.vb
+++ RenovacionInduccion/MainH.vb
@@ -1,9 +1,19 @@
 ﻿Public Class MainH
-    Public SelectedNumber(3) As Byte
-    Public NumberIndex As Byte = 1
+    Public SelectedNumber As String
+    Public PerformedSequences(49) As Byte
+    Public NumberIndex As Byte = 0
+    Public SequenceIndex As Byte = 0
     Private Sub Button_Click(sender As Object, e As EventArgs) Handles Button1.Click, Button2.Click, Button3.Click, Button4.Click
-        SelectedNumber(NumberIndex) = sender.Name
-        NumberIndex += 1
+        SelectedNumber = SelectedNumber & sender.Text
+        If Len(SelectedNumber) = 4 Then
+            PerformedSequences(SequenceIndex) = SelectedNumber
+            WriteLine(1, SelectedNumber)
+            SelectedNumber = ""
+            SequenceIndex += 1
+        End If
+        If SequenceIndex = 50 Then
+            End
+        End If
     End Sub
 
 End Class
